a3af5485485ae62c0f0fd65ccda255151d6e6826
Ren712 <reni.tijeras@gmail.com>
2025-05-12 17:33:30 -0300

    esto es una prueba para despegar

---
+++ poo.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+import math 
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def calulate_circle_area(self):
+        return math.pi * self.radius**2
+
+    def calulate_circle_perimeter(self):
+        return math.pi * self.radius *2
+
+radius= (float(input("Input the radius of the circle: ")))
+circle= Circle(radius)
+area=circle.calulate_circle_area()
+print("Area:" , area)
+perimeter=circle.calulate_circle_perimeter()
+print("Perimeter" , perimeter)
